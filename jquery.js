383c4ea45d535d7a5d2a8e1ff07e88df04f11aaa
root <root@maxdavidking.com>
2016-04-11 12:53:06 -0400

    added jquery scroll functionality

--- jquery.js
+++ jquery.js
@@ -1,5 +1,17 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
 	$("button").click(function (){
 		$("p").toggle();
   });
 });
+commented out for now - toggle above works */
+
+// Script below works. Need to add on scroll functionality. Need to name(rename) row divs to make them selectable (eg: row1, row2, row3, etc...) 
+// Also need to add ability to get back to home.
+$(document).ready(function () {
+	$("button").click(function() {
+		$('html, body').animate({
+			scrollTop: $(".row3").offset().top
+		}, 2000);
+	});
+});
+
