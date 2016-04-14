1d26cfe33237dc4cc9325067b58169e10f6366df
root <root@maxdavidking.com>
2016-04-14 10:19:10 -0400

    added jquery (and html and CSS) to get and post pages of data at the bottom of the page - still needs refinement

--- jquery.js
+++ jquery.js
@@ -7,7 +7,9 @@ commented out for now - toggle above works */
 
 // Script below works. Need to add on scroll functionality. Need to name(rename) row divs to make them selectable (eg: row1, row2, row3, etc...) 
 // Also need to add ability to get back to home.
-$(document).ready(function () {
+
+//Removed for now. Attempting with jQuery get requests
+/*$(document).ready(function () {
 	$("button").click(function() {
 		$('html, body').animate({
 			scrollTop: $(".row2").offset().top
@@ -15,3 +17,32 @@ $(document).ready(function () {
 	});
 });
 
+
+//jQuery get
+$(document).ready(function () {
+	$("button").click(function() {
+		$.get( "ajax/test.html", function( data ) {
+			$( ".row2" ).html( data );
+		});
+	});
+});
+	
+$(document).ready(function () {
+	$("button").click(function(){
+		$.get("ajax/test.html", function(data, status){
+			alert("Data: " + data + "\nStatus: " + status);
+		});
+	});
+});
+*/
+$(document).ready(function () {
+	$("button").click(function(){
+var posting = $.post("ajax/test.html", function(data){
+  var mainDiv = $(".row2", data); // finds <div id='mainDiv'>...</div>
+}, "html");
+posting.done(function( data ) {
+    var content = $( data ).find( ".row2" );
+    $( ".result" ).empty().append( content );
+	});
+  });
+});
