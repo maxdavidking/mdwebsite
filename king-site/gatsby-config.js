b35bd363e4df106377a2a04fd27d801485b27429
maxdavidking <maxdavidking@gmail.com>
2020-02-05 11:34:59 -0500

    Update gatsby config

--- king-site/gatsby-config.js
+++ king-site/gatsby-config.js
@@ -7,7 +7,18 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-eslint',
+      {
+       resolve: 'gatsby-plugin-eslint',
+       options: {
+         test: /\.js$|\.jsx$/,
+         exclude: /(node_modules|.cache|public)/,
+         stages: ['develop'],
+         options: {
+           emitWarning: true,
+           failOnError: false
+         }
+       }
+     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
