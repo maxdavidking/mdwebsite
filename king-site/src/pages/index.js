91e9b459d2ae15aaddeb246643f9456700e6d5a2
maxdavidking <maxdavidking@gmail.com>
2020-02-05 11:32:57 -0500

    Add font import to helmet

--- king-site/src/pages/index.js
+++ king-site/src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-family: Rubik;
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <H1>Hi people</H1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </Layout>
