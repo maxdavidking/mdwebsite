d13026741876bcf207c8558ab7efb726308845e1
maxdavidking <maxdavidking@gmail.com>
2020-02-05 15:52:25 -0500

    Add basic styling (color/font/weight)

--- king-site/src/pages/404.jsx
+++ king-site/src/pages/404.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+
+const Header = styled.h1`
+  font-family: Rubik;
+  font-weight: 700;
+  line-height: 1.4;
+  color: #FF0032;
+`;
+
+const Body = styled.p`
+  font-family: Rubik;
+  font-weight: 400;
+  line-height: 1.4;
+  color: #000000;
+`;
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Header>NOT FOUND</Header>
+    <Body>You just hit a route that doesn&#39;t exist... the sadness.</Body>
   </Layout>
 );
 
--- king-site/src/pages/index.jsx
+++ king-site/src/pages/index.jsx
@@ -3,16 +3,27 @@ import styled from 'styled-components';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const H1 = styled.h1`
+const Header = styled.h1`
   font-family: Rubik;
+  font-weight: 700;
+  line-height: 1.4;
+  color: #FF0032;
+`;
+
+const Body = styled.p`
+  font-family: Rubik;
+  font-weight: 400;
+  line-height: 1.4;
+  color: #000000;
 `;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <H1>Hi people</H1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Header>Max King</Header>
+    <Body>
+      Full Stack Developer at CNYCN in New York City. Interested in Running and Strava&#39;s API.
+    </Body>
   </Layout>
 );
 
