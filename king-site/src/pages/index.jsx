a27deeb5337b3fdefa5b4a52a1d74cd98a1c258e
maxdavidking <maxdavidking@gmail.com>
2020-02-09 17:59:51 -0500

    Add inline SVG for animation

--- king-site/src/pages/index.jsx
+++ king-site/src/pages/index.jsx
@@ -16,34 +16,15 @@ const Link = styled.a`
   padding: 0 0.2em;
 `;
 
+const DivingContainer = styled.div`
+  width: 50%;
+  padding-top: 10%;
+`;
+
 const DivingSVG = (
-  <svg
-    className="arches-svg-node"
-    version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
-    viewBox="0 0 552.7 62.1"
-  >
-    <path
-      className="arches"
-      fill="none"
-      stroke="#ecf0f1"
-      strokeWidth="10"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeMiterlimit="10"
-      d="M505,55c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50S5,27.4,5,55"
-    />
-    <path
-      className="splash-lines"
-      fill="none"
-      stroke="#ecf0f1"
-      strokeWidth="6"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M549.7,46.6l-21.8,12.6 M531.9,25.8l-12.6,21.8 M504.2,18.3v25.1 M476.4,25.8L489,47.6 M458.7,46.6l21.8,12.6"
-    />
+  <svg className="arches-svg-node" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 552.7 62.1">
+    <path className="arches" fill="none" stroke="#10ABFD" strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M505,55c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50s-50,22.4-50,50c0-27.6-22.4-50-50-50S5,27.4,5,55" />
+    <path className="splash-lines" fill="none" stroke="#10ABFD" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" d="M549.7,46.6l-21.8,12.6 M531.9,25.8l-12.6,21.8 M504.2,18.3v25.1 M476.4,25.8L489,47.6 M458.7,46.6l21.8,12.6" />
   </svg>
 );
 
@@ -51,7 +32,9 @@ const IndexPage = () => (
   <Layout>
     <GlobalStyles />
     <SEO title="Home" />
-    <DivingSVG />
+    <DivingContainer>
+      {DivingSVG}
+    </DivingContainer>
     <Header>Max King</Header>
     <Body>
       Full Stack Developer at
